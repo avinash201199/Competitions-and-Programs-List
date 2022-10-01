fbd326ac5a327a7d248bb0b5431bed02d69c240c
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-10-01 14:33:04 +0530

    initial commit

---
+++ site/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      fgm
+    </div>
+  );
+}
+
+export default App;
---
+++ site/src/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline:0;
+        box-sizing:border-box;
+        font-family: 'Open Sans', sans-serif; 
+    }
+    #root{
+        margin:0 auto;
+    }
+`
